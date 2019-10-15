9ef11491cd09a8b84a081e38df027a41ef5c5e82
MinjaeLyou <lminjae0313@gmail.com>
2019-10-15 22:33:36 +0900

    [ADD] Face routing setting

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var faceRouter = require('./routes/face');
 
 var sequelize = require('./models').sequelize;
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/face', faceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,6 +42,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-sequelize.sync();
+//sequelize.sync();
 
 module.exports = app;
