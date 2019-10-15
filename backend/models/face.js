295a268cafa1503432b3c9599c5d8e11a08a813c
MinjaeLyou <lminjae0313@gmail.com>
2019-10-15 21:52:19 +0900

    [ADD] Models

---
+++ backend/models/face.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const face = sequelize.define('face', {
+      index: {
+        type: DataTypes.INTEGER(11),
+        autoIncrement: true,
+        primaryKey: true,
+        comment: '인덱스'
+      },
+      posOrNeg: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        comment: '긍정/부정'
+      },
+      result: {
+        type: DataTypes.FLOAT(10),
+        allowNull: false,
+        comment: '인식 척도'
+      }
+    }, {
+      tableName: 'face',
+      comment: '인식 결과',
+      charset: 'utf8',
+      collate: 'utf8_general_ci'
+    });
+    return face;
+  };
+  
---
+++ backend/models/user.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define('user', {
+      id: {
+        type: DataTypes.INTEGER(11),
+        autoIncrement: true,
+        primaryKey: true,
+        comment: '인덱스'
+      },
+      userId: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        comment: '사용자 아이디'
+      },
+      password: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        comment: '사용자 비밀번호'
+      },
+      name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        comment: '사용자 이름'
+      },
+      manufacturer: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        comment: '제휴사'
+      }
+    }, {
+      tableName: 'user',
+      comment: '유저',
+      charset: 'utf8',
+      collate: 'utf8_general_ci'
+    });
+    user.associate = function (models) {
+      user.hasMany(models.face, { foreignKey: { name: 'userId', allowNull: false}});
+    };
+    return user;
+  };
+  
