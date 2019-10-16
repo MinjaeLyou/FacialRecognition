3c87af550fb6d80514dd584ec212da47bb2703d3
MinjaeLyou <lminjae0313@gmail.com>
2019-10-17 01:08:32 +0900

    [ADD] Get user information routing to backend folder
    Add new server routing.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const wrap = require('express-async-wrap');
+const models = require('../models');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/getUser/:id', wrap(async (req, res) => {
+  console.log(req.params.id)
+  const user = await models.users.findOne({
+    where: {
+      userId: req.params.id
+    }
+  });
+  res.send(user);
+}));
 
 module.exports = router;
