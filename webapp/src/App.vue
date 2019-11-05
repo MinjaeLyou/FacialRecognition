b62a219272b023a2959702d8e87a831734a504f3
MinjaeLyou <lminjae0313@gmail.com>
2019-11-06 01:52:02 +0900

    [INITIAL] Webapp

---
+++ webapp/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/facialr.png" width="200px">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: white;
+  width: 800px;
+  height: 600px;
+  margin: 100px auto;
+  border: 1px solid #bcbcbc;
+  border-radius: 3%;
+}
+
+img{
+  margin-top: 15px;
+}
+
+body {
+  background-color: #E6E6E6;
+}
+</style>
---
+++ webapp/src/components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="app flex-row align-items-center">
+    <div class="container">
+      <b-row class="justify-content-center">
+        <b-col md="8">
+          <b-card-group>
+            <b-card no-body class="p-4">
+              <b-card-body>
+                <b-form>
+                  <h2>로그인</h2>
+                  <p class="text-muted">Sign In to your account</p>
+                  <b-input-group class="mb-3">
+                    <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
+                    <b-form-input type="text" class="form-control" placeholder="아이디" autocomplete="username email" v-model="uid"/>
+                  </b-input-group>
+                  <b-input-group class="mb-4">
+                    <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
+                    <b-form-input type="password" class="form-control" placeholder="비밀번호" autocomplete="current-password" v-model="password"/>
+                  </b-input-group>
+                  <b-row>
+                    <b-col cols="6">
+                      <b-button variant="primary" class="px-4" v-on:click="login()">Login</b-button>
+                    </b-col>
+                    <b-col cols="6">
+                      <b-button variant="primary" class="px-4" v-on:click="login()">Register</b-button>
+                    </b-col>
+                  </b-row>
+                </b-form>
+              </b-card-body>
+            </b-card>
+            
+          </b-card-group>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      uid: '',
+      pw: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
