a93385bc3c420596f16d938873ebb2439fc9603f
MinjaeLyou <lminjae0313@gmail.com>
2019-11-06 16:37:56 +0900

    [ADD] Register function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,6 +19,29 @@ router.get('/getUser/:id', wrap(async (req, res) => {
   res.send(user);
 }));
 
+router.post('/register', wrap(async (req, res) => {
+  console.log(req.body);
+  console.log(req.body.uid);
+  const user = await models.users.create({ // 유저 검색
+      
+      userId: req.body.userId,
+      password: req.body.password
+  });
+  console.log(user);
+  if(user) {
+          delete req.body.password;
+          res.send({ // 로그인 결과 response
+              result: true,
+              data: user
+          });
+      } else {
+          res.send({
+              result: false
+          });
+      }
+  
+}));
+
 router.post('/login', wrap(async (req, res) => {
   console.log(req.body);
   console.log(req.body.uid);
